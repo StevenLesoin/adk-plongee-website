cf025c565e3fde090289becfaa6ee018b51f7abf
tra-ma-sea-rider <63953749+tra-ma-sea-rider@users.noreply.github.com>
2020-04-21 09:19:12 +0200

    Ajout d'une plongée et liste des sorties
    Ajout d'un événement sans champs pré remplis en format texte, formulaire intégré dans la page et affichage des erreurs de saisie.
    Consultation des événements sur page dédiée

---
+++ home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>ADK plongée</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+
+<body>
+
+	<?php 
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=adk_plongee_website;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}
+	catch (Exception $e)
+	{
+	    die('Erreur : ' . $e->getMessage());
+	}
+
+	$pseudo = htmlspecialchars($_POST['login']);
+	//  Récupération de l'utilisateur et de son pass hashé
+	$req = $bdd->prepare('SELECT id, mdp, privilege FROM membres WHERE pseudo = :pseudo');
+	$req->execute(array(
+	    'pseudo' => $pseudo));
+	$resultat = $req->fetch();
+
+	// Comparaison du mdp envoyé via le formulaire avec la base
+	$isPasswordCorrect = password_verify($_POST['password'], $resultat['mdp']);
+
+	if (!$resultat)
+	{
+		include("tools/navbar.php"); 
+		include("tools/login_failure.php");
+	}
+	else
+	{
+	    if ($isPasswordCorrect) {
+	        session_start();
+	        $_SESSION['id'] = $resultat['id'];
+	        $_SESSION['pseudo'] = $pseudo;
+	        $_SESSION['privilege'] = $resultat['privilege'];
+	        include("tools/navbar.php"); 
+			include("tools/login_success.php");
+	    }
+	    else {
+	    	include("tools/navbar.php"); 
+	        include("tools/login_failure.php");
+	    }
+	}
+
+	$req->closeCursor(); //requête terminée
+	?>
+
+  <?php include("tools/footer.php"); ?>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+
+
+</html>
---
+++ login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>ADK plongée login</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+
+<body>
+
+<?php include("tools/navbar.php"); ?>
+
+<?php
+/*$pass_hache = password_hash("LucieCarof1*", PASSWORD_DEFAULT);
+echo $pass_hache; */
+?>
+
+  <div class="section no-pad-bot" id="index-banner">
+  	<div class="container">
+    	<br><br>
+        <div class="row center">
+            <span class="flow-text" col s12">Indentifiez-vous pour accéder à l'espace membre :</span>
+            </div>
+            <div class="row center">
+                <form class="col s12" action="home.php" method="post">
+                    <div class="row center">
+                        <div class="input-field col s6">
+                            <i class="material-icons prefix">account_circle</i>
+                            <input id="icon_prefix" type="text" class="validate" name="login">
+                            <label for="icon_prefix">Pseudo</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <i class="material-icons prefix">https</i>
+                            <input id="password" type="password" class="validate" name='password'>
+                            <label for="password">Mot de passe</label>
+                        </div>
+                    </div>
+                    <div class="row center">
+                        <button class="btn waves-effect waves-light blue darken-4" type="submit" name="submit">Se connecter</button> 
+                    </div>
+                    <div class="row center">
+                        <a class="waves-effect waves-light btn blue darken-4" href="registration.php">Créer un compte</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+  	</div>
+  </div>
+
+  <?php include("tools/footer.php"); ?>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+</body>
+
+
+</html>
