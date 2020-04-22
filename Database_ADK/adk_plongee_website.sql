e231cc12fa45c63ce045e81a19df44f7aa9bb7bc
tra-ma-sea-rider <63953749+tra-ma-sea-rider@users.noreply.github.com>
2020-04-22 23:48:34 +0200

    Add files via upload

--- Database_ADK/adk_plongee_website.sql
+++ Database_ADK/adk_plongee_website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 21 avr. 2020 à 19:20
+-- Généré le :  mer. 22 avr. 2020 à 21:43
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `evenements`;
 CREATE TABLE IF NOT EXISTS `evenements` (
+  `id` int(11) NOT NULL,
   `type` text NOT NULL,
   `titre` text NOT NULL,
   `date_evt` date NOT NULL,
@@ -40,18 +41,43 @@ CREATE TABLE IF NOT EXISTS `evenements` (
   `lieu` text NOT NULL,
   `remarques` text NOT NULL,
   `pseudo` text NOT NULL,
-  `date_publi` text NOT NULL
+  `date_publi` text NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `evenements`
 --
 
-INSERT INTO `evenements` (`type`, `titre`, `date_evt`, `heure_evt`, `date_lim`, `heure_lim`, `niveau_min`, `lieu`, `remarques`, `pseudo`, `date_publi`) VALUES
-('Plongée', 'Sortie de nuit !', '2020-04-25', '00:00:01', '2020-04-24', '19:00:00', 'N1', 'Mer', 'Lampe obligatoire', 'Vide', 'Vide'),
-('Plongée', 'Plongée 2', '2020-04-26', '00:00:01', '2020-04-25', '17:00:00', 'N2', 'Mer', 'Du bord :-(', 'Vide', 'Vide'),
-('Plongée', 'Test 20:00', '2020-05-12', '00:00:01', '2020-05-12', '19:00:00', 'N1', 'Mer', '', 'Vide', 'Vide'),
-('Plongée', 'Plongée de l\'année sur le Kleber', '2020-07-01', '08:15:00', '2020-06-30', '21:00:00', 'N3', 'Mer', 'Pour ceux préparés aux profondes avant', 'Vide', 'Vide');
+INSERT INTO `evenements` (`id`, `type`, `titre`, `date_evt`, `heure_evt`, `date_lim`, `heure_lim`, `niveau_min`, `lieu`, `remarques`, `pseudo`, `date_publi`) VALUES
+(1, 'Plongée', 'Sortie de nuit !', '2020-04-25', '00:00:01', '2020-04-24', '19:00:00', 'N1', 'Mer', 'Lampe obligatoire', 'Vide', 'Vide'),
+(2, 'Plongée', 'Plongée 2', '2020-04-26', '00:00:01', '2020-04-25', '17:00:00', 'N2', 'Mer', 'Du bord :-(', 'Vide', 'Vide'),
+(3, 'Plongée', 'Test 20:00', '2020-05-12', '00:00:01', '2020-05-12', '19:00:00', 'N1', 'Mer', '', 'Vide', 'Vide'),
+(4, 'Plongée', 'Plongée de l\'année sur le Kleber', '2020-07-01', '08:15:00', '2020-06-30', '21:00:00', 'N3', 'Mer', 'Pour ceux préparés aux profondes avant', 'Vide', 'Vide'),
+(5, 'Plongée', 'er', '2020-02-02', '20:00:00', '2020-02-02', '20:20:00', 'N1', 'Mer', '', 'Vide', 'Vide');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscriptions`
+--
+
+DROP TABLE IF EXISTS `inscriptions`;
+CREATE TABLE IF NOT EXISTS `inscriptions` (
+  `id_evt` int(11) NOT NULL,
+  `id_adhérent` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `inscriptions`
+--
+
+INSERT INTO `inscriptions` (`id_evt`, `id_adhérent`) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(1, 3),
+(3, 1);
 
 -- --------------------------------------------------------
 
