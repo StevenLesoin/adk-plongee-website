551986dff82809cb7b5132f791dcd0a8a6ce8947
Clément <clement.mahe@gmail.com>
2020-04-27 21:47:18 +0200

    Essai de modif mineur
    Pour test

--- home.php
+++ home.php
@@ -19,6 +19,7 @@ session_start()
 <body>
 
 	<?php 
+	// test
 	if(isset($_SESSION['pseudo']) AND isset($_SESSION['privilege'])) // Si déjà connecté
     {
     	if($_SESSION['oubli_mdp'] == 1) // Si connexion avec mot de passe temporaire
