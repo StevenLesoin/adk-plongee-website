57eb4095fef89cc7368a55c08927a60f0f926446
Steven Lesoin <steven.lesoin@gmail.com>
2020-04-27 19:15:19 +0200

    Cleaning code

--- tools/registration_failure.php
+++
@@ -1,9 +0,0 @@
-<div class="section no-pad-bot" id="index-banner">
-    <div class="container">
-    <br><br>
-	<div class=" row center">
-		<div class="col s12">
-		    <span class="flow-text" col s12">Un ou plusieurs champs n'ont pas été remplis correctement !</span>
-		</div>
-	</div>
-</div>
