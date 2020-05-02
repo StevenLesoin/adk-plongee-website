f073cb45f6bc3d792c733c5205c5821788f5a4ea
Steven Lesoin <steven.lesoin@gmail.com>
2020-05-02 19:37:34 +0200

    Adding mobile navbar

--- tools/navbar.php
+++ tools/navbar.php
@@ -17,24 +17,26 @@
   
   
   <nav class="blue darken-4" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="index.php" class="brand-logo">ADK-plongée</a>
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="index.php" class="brand-logo">ADK-plongée</a>
+      <a href="#" data-target="mobile-navbar" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
                 <!-- Dropdown Trigger -->
-        <li><a class="dropdown-button" href="#!" data-activates="club_navBar">Le club<i class="material-icons right">arrow_drop_down</i></a></li>
+        <li><a class="dropdown-trigger" href="#!" data-target="club_navBar">Le club<i class="material-icons right">arrow_drop_down</i></a></li>
         <li><a href="#">Contact</a></li>
 
         <?php  
-			// Affichage pour les membres
+			    // Affichage pour les membres
           if(isset($_SESSION['pseudo']) AND isset($_SESSION['privilege'])) // Si membre
           {
             ?>
-			      <li><a class="dropdown-button" href="#!" data-activates="outils_navBar">Outils<i class="material-icons right">arrow_drop_down</i></a></li>
+			      <li><a class="dropdown-button" href="#!" data-target="outils_navBar">Outils<i class="material-icons right">arrow_drop_down</i></a></li>
             <?php   
-			// Affichage pour les Admins en plus des membres
+			      // Affichage pour les Admins en plus des membres
             if($_SESSION['privilege']='administrateur') // Si admin
             {
             ?>
-				      <li><a class="dropdown-button" href="#!" data-activates="outils_admin_navBar">Outils Admin<i class="material-icons right">arrow_drop_down</i></a></li>
+				      <li><a class="dropdown-button" href="#!" data-target="outils_admin_navBar">Outils Admin<i class="material-icons right">arrow_drop_down</i></a></li>
             <?php 
             }
             ?>
@@ -51,8 +53,44 @@
         ?>
 
       </ul>
-      <ul id="nav-mobile" class="sidenav">
-      </ul>
-      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
   </nav>
+
+
+    <ul class="sidenav" id="mobile-navbar">
+      <li><a href="#">Historique</a></li>
+      <li><a href="#">Le club en chiffres</a></li>
+      <li><a href="liste_evenements.php">Liste des événements</a></li>
+      <li><a href="fonctionnement_site.php">FAQ / Fonctionnement du site</a></li>
+
+      <li><a href="#">Contact</a></li>
+
+      <?php  
+          // Affichage pour les membres
+          if(isset($_SESSION['pseudo']) AND isset($_SESSION['privilege'])) // Si membre
+          {
+            ?>
+            <li><a href="liste_evenements.php">Liste des événements</a></li>
+            <li><a href="creation_evt.php">Ajout d'événement</a></li>
+            <?php   
+            // Affichage pour les Admins en plus des membres
+            if($_SESSION['privilege']='administrateur') // Si admin
+            {
+            ?>
+              <li><a href="suppr_evt.php">Suppression d'événements</a></li>
+              <li><a href="admin.php">Infos membres</a></li>
+            <?php 
+            }
+            ?>
+            <li><a href="logout.php">Déconnexion</a></li>
+            <li><a href="home.php"><?php echo $_SESSION['pseudo']; ?></a></li>
+          <?php 
+          }
+          else // Si non membre
+          {
+            ?>
+            <li><a href="login.php">Accès membre</a></li>
+            <?php 
+          }
+        ?>
+    </ul>
