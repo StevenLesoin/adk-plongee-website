e3e71f37bef0af3113db85841aa6a52f0fd65776
Steven Lesoin <steven.lesoin@gmail.com>
2020-05-07 19:14:00 +0200

    Set version number to footer

--- tools/footer.php
+++ tools/footer.php
@@ -1,7 +1,8 @@
   <footer class="page-footer blue darken-4">
     <div class="footer-copyright">
       <div class="container">
-      Made by Steven LESOIN & Clément MAHE
+      	V1.0.0 May 2020
+      	<a class="grey-text text-lighten-4 right" href="#!">Made by Steven LESOIN & Clément MAHE</a>
       </div>
     </div>
   </footer>
